ec0045591cc0d2484d7c7f96c3ecd17f2f6bf2b7
Pere Esteve <pesteve@BAR3-331.local>
2019-01-08 10:07:15 +0100

    First loop and appending of images

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 module.exports = function gridllery(args) {
+  /*
+  containerClass:'gridllery',
+  numberoffotos:106,
+  folder:"static/",
+  bigFolder:"big/",
+  smallFolder:"small/"
+  */
   console.log(args)
-};
+  var container = document.getElementsByClassName(args.containerClass)
+
+  var i;
+  for (i = 0; i < args.numberoffotos; i++) {
+    var img = document.createElement('img');
+    img.src = args.folder+args.smallFolder+i+'.jpg'
+    container.appendChild(img);
+  }
+
+}
