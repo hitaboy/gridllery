bef1e8a758d75e4849018faa7504109c8bedcb57
Pere Esteve <pesteve@BAR3-331.local>
2019-01-08 15:33:10 +0100

    Git and webpack update

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ var gridllery = function gridllery(args) {
   smallFolder:"small/"
   */
   console.log(args)
-  var $container = $(args.containerClass)
+  var $container = $('.'+args.containerClass)
   console.log($container)
   var i;
   for (i = 1; i < (args.numberoffotos+1); i++) {
     var $img = $('<img class="block">')
     $img.attr('src', args.folder+args.smallFolder+i+'.jpg')
     $img.data('big', args.folder+args.bigFolder+i+'.jpg')
-    $container.append(img)
+    $container.append($img)
   }
-  $container.find('block').on('click',function(){
-    console.log($(this).data('big'))
+
+  $('<div class="big"></div>').appendTo('body')
+
+  $container.find('.block').on('click',function(){
+    var big = $(this).data('big')
   })
 }
 
